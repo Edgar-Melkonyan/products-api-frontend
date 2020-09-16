28e358f65e562d0bd542143c63de5949d81c2fc8
Edgar-Melkonyan <edgar365224@gmail.com>
2020-09-16 12:59:06 +0400

    make TableRow functional component

--- src/components/Products/TableRow.js
+++ src/components/Products/TableRow.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 
-export default class ProductTableRow extends Component {
-    render() {
-        return (
-            <tr>
-                <td>{this.props.obj.name}</td>
-                <td>{this.props.obj.price}</td>
-                <td>{this.props.obj.description}</td>
-                <td>
-                    <Link className="edit-link" to={"/edit-product/" + this.props.obj.id}>
-                       <Button size="sm" variant="info">Edit</Button>
-                    </Link>
-                    <Button onClick={this.props.handleDelete} size="sm" variant="danger">
-                        Delete
-                    </Button>
-                </td>
-            </tr>
-        );
-    }
-}
+export default props => (
+    <tr>
+        <td>{props.obj.name}</td>
+        <td>{props.obj.price}</td>
+        <td>{props.obj.description}</td>
+        <td>
+            <Link className="edit-link" to={"/edit-product/" + props.obj.id}>
+                <Button size="sm" variant="info">Edit</Button>
+            </Link>
+            <Button onClick={props.handleDelete} size="sm" variant="danger">
+                Delete
+            </Button>
+        </td>
+    </tr>
+);
